421a7fffeafb84793ae63006df7d1f2f6ac9721e
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2024-10-04 16:11:32 +0530

    Update Home.jsx

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
     const fetchUserIdByName = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:8080/api/user/by-name/${name}`
+          `https://final-deploy-1.onrender.com/user/by-name/${name}`
         );
         setUserId(response.data.userId);
       } catch (error) {
